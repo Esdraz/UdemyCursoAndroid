be56389df3d4950d542aa7bcd9a4a5e32016a95a
Francisco <fsousa.jpa@gmail.com>
2024-06-15 20:00:36 -0300

    add exemplo sobrecarga de construtores e metodos

--- src/main/java/com/esdraz/poo/construtor/ExemploConstrutores.java
+++ src/main/java/com/esdraz/poo/construtor/ExemploConstrutores.java
@@ -4,8 +4,11 @@ public class ExemploConstrutores {
 
     public static void main(String[] args) {
         Usuario usuario = new Usuario("francisco@gmail.com", "mudar@123");
+        Usuario usuarioTelefone = new Usuario("558899999999");
+
+        usuario.logar("jose@gmail.com", "meuacesso123");
 
         System.out.printf("Usuário: %s\nSenha: %s\n", usuario.email, usuario.senha);
-        usuario.verificarUsuarioLogado();
+        System.out.printf("Usuário: %s\n", usuarioTelefone);
     }
 }
--- src/main/java/com/esdraz/poo/construtor/Usuario.java
+++ src/main/java/com/esdraz/poo/construtor/Usuario.java
@@ -5,13 +5,26 @@ public class Usuario {
     String email;
     String senha;
 
+    //sobrecarga de construtores
     public Usuario(String email, String senha) {
-        System.out.println("Executando o construtor");
+        System.out.println("Autenticação por e-mail e senha");
         this.email = email;
         this.senha = senha;
+    }
+    public Usuario(String telefone) {
+        System.out.println("Autenticação por telefone");
     }
 
     void verificarUsuarioLogado() {
         System.out.println("Usuário logado com sucesso!");
+    }
+
+    //sobrecarga de métodos
+    void logar(String email, String senha) {
+        System.out.println("Autenticando com e-mail e senha");
+    }
+
+    void logar(String telefone) {
+        System.out.println("Autenticando com telefone");
     }
 }
